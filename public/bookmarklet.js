50862e3064ebf552455432e4eed21795cd402d87
Jerod Santo <jerod.santo@gmail.com>
2012-09-19 10:46:44 -0500

    more bookmarklet style fixes

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -4,14 +4,57 @@
     return;
   }
 
+  var pc = screen.width >= 1024;
+  var phone = screen.width <= 700;
+  var landscape = Math.abs(window.orientation) == 90;
+  var maxFontSize = pc ? 48 : 3600;
+  var maxFontRatio = phone ? (landscape ? 6 : 8) : (landscape ? 10 : 14);
+
   var d = document;
   var req = new XMLHttpRequest();
   var data = new FormData();
-  var overlay = '<div id="pp-overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: black; opacity: 0.875; z-index: 2147483647; padding: 147px 0 0; -webkit-text-size-adjust: none;"><h1 id="pp-message" style="font-size: 40px; font-family: Helvetica, sans-serif; color: #ccc; text-align: center;">Pushing...</h1></div>';
-  var container = d.createElement("div");
-  container.setAttribute("style", "position: inherit;");
-  container.innerHTML = overlay;
-  d.body.appendChild(container);
+
+  var overlay = d.createElement("div");
+  overlay.setAttribute("id", "pp-overlay");
+
+  overlay.style.position = "fixed";
+  overlay.style.zIndex = 2147483647;
+  overlay.style.left = 0;
+  overlay.style.top = 0;
+  overlay.style.width = "100%";
+  overlay.style.height = "100%";
+  overlay.style.opacity = 0.9;
+  overlay.style.webkitTransition = "opacity 0.25s linear";
+  overlay.style.webkitTextSizeAdjust = "none";
+  overlay.style.textAlign = "center";
+  overlay.style.padding = "147px 0px 0px";
+  overlay.style.margin = "0px";
+  overlay.style.backgroundColor = "rgb(0, 0, 0)";
+  overlay.style.color = "rgb(204, 204, 204)";
+  overlay.style.fontFamily = "Helvetica, Arial, sans-serif";
+  overlay.style.fontWeight = "bold";
+  overlay.style.lineHeight = 1;
+  overlay.style.letterSpacing = "normal";
+  overlay.style.fontVariant = "normal";
+  overlay.style.fontStyle = "normal";
+  overlay.style.fontSize = Math.min(maxFontSize, Math.floor(window.innerHeight / maxFontRatio)) + "px";
+  overlay.style.paddingTop = Math.floor(window.innerHeight / 3) + "px";
+
+  var message = d.createElement("span");
+  message.setAttribute("id", "pp-message");
+  message.style.webkitTextSizeAdjust = "none";
+  message.style.fontFamily = "Helvetica, Arial, sans-serif";
+  message.style.fontWeight = "bold";
+  message.style.lineHeight = 1;
+  message.style.letterSpacing = "normal";
+  message.style.fontVariant = "normal";
+  message.style.fontStyle = "normal";
+
+  message.innerHTML = "Pushing...";
+
+  overlay.appendChild(message);
+  d.body.appendChild(overlay);
+
   data.append("url", encodeURIComponent(d.location.href));
   req.open("POST", PushPop.server + "/push/" + PushPop.token, true);
   req.onreadystatechange = function(event) {
